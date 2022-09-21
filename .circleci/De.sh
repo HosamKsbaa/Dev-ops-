7a1a4c124b0fd870c9662ae33d060b8644616f12
HosamKsbaa <35602111+HosamKsbaa@users.noreply.github.com>
2022-09-21 14:09:43 +0200

    d

--- .circleci/De.sh
+++ .circleci/De.sh
@@ -1,3 +1,4 @@
-aws --region us-east-1 s3 rm s3://udapeople-3e9f3ed --recursive
-aws --region us-east-1 cloudformation delete-stack --stack-name udapeople-backend-3e9f3ed
-aws --region us-east-1 cloudformation delete-stack --stack-name udapeople-frontend-3e9f3ed
+aws --region us-east-1 s3 rm s3://udapeople-418cdd0 --recursive
+aws --region us-east-1 cloudformation delete-stack --stack-name udapeople-backend-418cdd0
+aws --region us-east-1 cloudformation delete-stack --stack-name udapeople-frontend-418cdd0 
+
